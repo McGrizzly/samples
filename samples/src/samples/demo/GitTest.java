39ff59d20f183ed39865ba65f9355032d482f6ae
amira <amira@LAPTOP-USVGF7NH>
2022-03-14 22:10:32 +0800

    Second Commit

--- samples/src/samples/demo/GitTest.java
+++ samples/src/samples/demo/GitTest.java
@@ -3,6 +3,7 @@ package samples.demo;
 public class GitTest {
 	public static void main(String[] args) {
         System.out.println("Git is Fun");
+        System.out.println("Git is Cool");
     }
 
 }
